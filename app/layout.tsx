28a94e6a27a4a757cb6d8f8e93f228f8d081f24a
bismay70 <samalbismay5@gmail.com>
2025-12-14 08:08:37 +0530

    page and layout in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,72 +1,44 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-import '../globals.css'
-import ClientProviders from '@/components/shared/client-providers'
-import { getDirection } from '@/i18n-config'
-import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
-import { routing } from '@/i18n/routing'
-import { notFound } from 'next/navigation'
-import { getSetting } from '@/lib/actions/setting.actions'
-import { cookies } from 'next/headers'
+import type { Metadata } from "next";
+import "./globals.css";
+import Layout from "@/components/Layout";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+export const metadata: Metadata = {
+  title: "Amazon online shopping",
+  description: "Amazon clone for learning purpose",
+};
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
-
-export async function generateMetadata() {
-  const {
-    site: { slogan, name, description, url },
-  } = await getSetting()
-  return {
-    title: {
-      template: `%s | ${name}`,
-      default: `${name}. ${slogan}`,
-    },
-    description: description,
-    metadataBase: new URL(url),
-  }
-}
-
-export default async function AppLayout({
-  params,
+export default function RootLayout({
   children,
-}: {
-  params: { locale: string }
-  children: React.ReactNode
-}) {
-  const setting = await getSetting()
-  const currencyCookie = (await cookies()).get('currency')
-  const currency = currencyCookie ? currencyCookie.value : 'USD'
-
-  const { locale } = await params
-  // Ensuring that the incoming `locale` is valid
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if (!routing.locales.includes(locale as any)) {
-    notFound()
-  }
-  const messages = await getMessages()
-
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html
-      lang={locale}
-      dir={getDirection(locale) === 'rtl' ? 'rtl' : 'ltr'}
-      suppressHydrationWarning
-    >
-      <body
-        className={`min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <ClientProviders setting={{ ...setting, currency }}>
-            {children}
-          </ClientProviders>
-        </NextIntlClientProvider>
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
+        />
+      </head>
+      <body className={`antialiased`}>
+        <Layout>
+          <Header />
+          {children}
+          <Footer />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                background: "#000000",
+                color: "#ffffff",
+              },
+            }}
+          />
+        </Layout>
       </body>
     </html>
-  )
+  );
 }
